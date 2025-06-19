0a5480a6451d08c0bd1901f849d3743fa17d6275
at-atanasov <at.atanasov@gmail.com>
2025-06-19 22:15:46 +0300

    Refs to Select DOM Elements Fic Cleanup Function

--- src/App.js
+++ src/App.js
@@ -217,7 +217,7 @@ function Search({ query, setQuery }) {
       }
 
       document.addEventListener("keydown", callback);
-      return () => document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
     },
     [setQuery]
   );
